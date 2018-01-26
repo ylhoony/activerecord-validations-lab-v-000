f995b693940218ecbb7932bad21ec54431f5620f
Hoon Lee <yl.hoony@gmail.com>
2018-01-26 05:01:49 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,12 +1,13 @@
 class Post < ActiveRecord::Base
   validates :title, presence: true
-  validate :clickbait?
+  validate :has_clickbait?
   validates :content, length: { minimum: 250 }
   validates :summary, length: { maximum: 250 }
   validates :category, inclusion: { in: ["Fiction", "Non-Fiction"] }
 
-  def clickbait?
-    !title.include?("Won't Believe") || !title.include?("Secret") || !title.include?("Guess") || (title =~ /([t][o][p]|[T][o][p])\s{1}\d+/).nil?
+  def has_clickbait?
+    clickbaits = [title.include?("Won't Believe"), title.include?("Secret"), title.include?("Guess"), !(title =~ /([t][o][p]|[T][o][p])\s{1}\d+/).nil?]
+    clickbaits.any {|clickbait| clickbait == true }
   end
 
 end
