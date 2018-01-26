3d380992dab838f307aaf350da5b6261b6b0d46d
Hoon Lee <yl.hoony@gmail.com>
2018-01-26 05:33:16 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,14 +2,21 @@ require 'pry'
 
 class Post < ActiveRecord::Base
   validates :title, presence: true
-  validate :must_have_clickbait
+  validate :is_clickbait?
   validates :content, length: { minimum: 250 }
   validates :summary, length: { maximum: 250 }
   validates :category, inclusion: { in: ["Fiction", "Non-Fiction"] }
 
-  def must_have_clickbait
+  CLICKBAIT_PATTERNS = [
+    /Won't Believe/i,
+    /Secret/i,
+    /Top [0-9]*/i,
+    /Guess/i
+  ]
+
+  def :is_clickbait?
     binding.pry
-    clickbaits = [title.include?("Won't Believe"), title.include?("Secret"), title.include?("Guess"), !(title =~ /([t][o][p]|[T][o][p])\s{1}\d+/).nil?]
+    clickbaits = [title.include?("Won't Believe"), title.include?("Secret"), title.include?("Guess"), !!(title =~ /([t][o][p]|[T][o][p])\s{1}\d+/)]
     if !clickbaits.include?(true)
       errors.add(:title, "must include clickbait-y")
     end
