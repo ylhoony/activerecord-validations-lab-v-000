b0619e0938f9e7d53b6503dd2e73956d8165c2e7
Hoon Lee <yl.hoony@gmail.com>
2018-01-26 05:14:51 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,6 +8,7 @@ class Post < ActiveRecord::Base
   validates :category, inclusion: { in: ["Fiction", "Non-Fiction"] }
 
   def must_have_clickbait
+    binding.pry
     clickbaits = [title.include?("Won't Believe"), title.include?("Secret"), title.include?("Guess"), !(title =~ /([t][o][p]|[T][o][p])\s{1}\d+/).nil?]
     if !clickbaits.include?(true)
       errors.add(:title, "must include clickbait-y")
