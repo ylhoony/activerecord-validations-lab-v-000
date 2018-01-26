737e09cd31833cb4959751de8ecc6cccb78f2c1a
Hoon Lee <yl.hoony@gmail.com>
2018-01-26 04:28:10 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,12 @@
 class Post < ActiveRecord::Base
   validates :title, presence: true
+  validate :clickbait?
   validates :content, length: { minimum: 250 }
   validates :summary, length: { maximum: 250 }
   validates :category, inclusion: { in: ["Fiction", "Non-Fiction"] }
+
+  def clickbait?
+    
+  end
+  
 end
